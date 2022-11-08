673557cd5cf1cb0b4c8b647b9e062bec3b59c935
vasylyeva12 <aloyna.vasilieva@gmail.com>
2022-11-08 22:51:34 +0200

    .

--- script.js
+++ script.js
@@ -5,43 +5,36 @@ let g = 140
 let b = 240
 console.log("rgb("+ r +", " + g +", " + b +" )")
 
-// let common = "rgb (+r+,'+g+','+b+')"
 
-// Задача 2
 
-// let number = prompt ("Введите число")
-// console.log(number);
-//    if (number > 100) {
-//         alert("Больше>")
-//     } else if(number == 100) {
-//         alert ("Равно")
-//     }
-//     else {
-//         alert("Меньше")
-//     }
+//  Задача 2
+
+let number = prompt ("Введите число")
+console.log(number);
+   if (number > 100) {
+        alert("Больше>")
+    } else if(number == 100) {
+        alert ("Равно")
+    }
+    else {
+        alert("Меньше")
+    }
 
 // Задача 3
 
-// let num1 = +prompt ('Введите число');
-// let num2 = +prompt ('Введите 2-е число');
-// let result = num1 + num2
-// console.log(result);
-// alert (num1 + num2);
+let num1 = +prompt ('Введите число');
+let num2 = +prompt ('Введите 2-е число');
+let result = num1 + num2
+let minus = num1 - num2
+let multiply = num1 * num2
+let devide = num1 / num2
 
-// let num1 = -prompt ('Введите число');
-// let num2 = -prompt ('Введите 2-е число');
-// let result = num1 - num2
-// console.log(result);
+console.log(result);
+console.log(minus);
+console.log(multiply);
+console.log(devide);
 
-// let num1 = prompt ('Введите число');
-// let num2 = prompt ('Введите 2-е число');
-// let result = num1 * num2
-// alert (num1 * num2);
 
-// let num1 = prompt ('Введите число');
-// let num2 = prompt ('Введите 2-е число');
-// let result = num1/num2
-// alert (num1/num2)
 
 
 
